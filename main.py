4864b3891359624f22fd0fb6213f2e95c07a8b50
Piotr Sierota <P.Sierota@stud.elka.pw.edu.pl>
2019-01-31 20:32:04 +0100

    Code cleaned up

--- main.py
+++ main.py
@@ -4,63 +4,70 @@ import server
 import numpy as np
 import matplotlib.pyplot as plt
 
-#AAP = [15, 20, 30, 50, 75, 100]  # 1/LAMBDA - average arrival periods [ms]
-AAP = [15, 25, 35, 45, 75, 150, 300]
+# CONFIGURATION
+AAP = [15, 25, 35, 45, 75, 150, 300]  # 1/LAMBDA - average arrival periods [ms]
 AST = 15  # 1/MI - average service time [ms]
-
-# LAMBDA = 1/AAP
-# MI = 1/AST
-
-PACKETS_AMOUNT = 1000000  # [packets]
-SERVER_NUMBER = 2
+NUMBER_OF_PACKETS = 100000  # [packets]
+NUMBER_OF_SERVERS = 2
 
 balancer = loadBalancer.LoadBalancer()
-
 aap_cases = []
+
 for i in range(len(AAP)):
     aap_cases.append(server.Server())
 
 # CREATING PACKETS
-for i in range(PACKETS_AMOUNT):
+for i in range(NUMBER_OF_PACKETS):
     packet = ipPacket.Packet(i)
     balancer.packet_list.append(packet)
 
-counter = 0
-server_worktime_list = []
 
+
+
+aap_counter = 0
+server_worktime_list = []
 server_uptime_worktime_ratio_list = []
 
-
 for aap_case_server in aap_cases:
+
+    # FOR EACH SERVER SETTING INITIAL VALUES
+    servers_release_times = []  # time of releasing the server
+    servers_work_times = []   # time of server being in working state
+    servers_idle_times = []  # time of server being in idle state
+    servers_worktime_to_uptime_ratio = []  # servers' worktime to uptime ratio
+    for i in range(NUMBER_OF_SERVERS):
+        servers_release_times.append(0)
+        servers_idle_times.append(0)
+
+    # ARRIVALS OF PACKETS
     arrival_time = 0  # time of packet arrival
-    server_release_time = [0, 0]  # time of releasing the server
-    server_idle_time = [0, 0]
-    server_worktime = []
-    server_worktime_uptime_ratio = []
-    # M/M/1 QUEUE SIMULATION
-    for i in range(PACKETS_AMOUNT):
-        interval = np.random.poisson(AAP[counter])  # interval between following packets
-        arrival_time += interval
-        if SERVER_NUMBER > 1:
-            server_number = int(round(np.random.random(SERVER_NUMBER - 1)[0]))
+    for i in range(NUMBER_OF_PACKETS):
+        aap_time = np.random.poisson(AAP[aap_counter])  # interval between following packets
+        arrival_time += aap_time
+
+        # CHOOSING THE SERVER THAT WILL RECEIVE THE PACKET
+        if NUMBER_OF_SERVERS > 1:
+            server_number = int(round(np.random.random(NUMBER_OF_SERVERS - 1)[0]))
         else:
             server_number = 0
-        if arrival_time >= server_release_time[server_number]: #dla danego serwera
-            server_idle_time[server_number] += (arrival_time - server_release_time[server_number])
+
+        if arrival_time >= servers_release_times[server_number]:  # dla danego serwera
+            servers_idle_times[server_number] += (arrival_time - servers_release_times[server_number])
             aap_case_server.consumePacket()
-            pst = np.random.exponential(AST)  # time of service the packet
-            server_release_time[server_number] = arrival_time + pst
+            pst = np.random.exponential(AST)  # packet service time
+            servers_release_times[server_number] = arrival_time + pst
         else:
             aap_case_server.dropPacket()
-    for server_number in range(SERVER_NUMBER):
-        server_worktime.append(server_release_time[server_number] - server_idle_time[server_number])
-        server_worktime_uptime_ratio.append(server_worktime[server_number]/server_release_time[server_number])
-        print("Server worktime/uptime ratio: " + str(server_worktime_uptime_ratio[server_number]))
-        print("Lambda/mi ratio: " + str(AST/AAP[counter]))
-    server_worktime_list.append(server_worktime)
-    server_uptime_worktime_ratio_list.append(server_worktime_uptime_ratio)
-    counter += 1
+    for server_number in range(NUMBER_OF_SERVERS):
+        servers_work_times.append(servers_release_times[server_number] - servers_idle_times[server_number])
+        servers_worktime_to_uptime_ratio.append(servers_work_times[server_number] / servers_release_times[server_number])
+        print("Server worktime/uptime ratio: " + str(servers_worktime_to_uptime_ratio[server_number]))
+        print("Lambda/mi ratio: " + str(AST / AAP[aap_counter]))
+    server_worktime_list.append(servers_work_times)
+    server_uptime_worktime_ratio_list.append(servers_worktime_to_uptime_ratio)
+    aap_counter += 1
 
+# RESULTS
 result_consumed_packets = []
 for i in range(len(aap_cases)):
     result_consumed_packets.append(aap_cases[i].packets_consumed)
@@ -71,13 +78,11 @@ for i in range(len(aap_cases)):
 
 loss_probability = []
 for i in range(len(aap_cases)):
-    loss_probability.append(result_dropped_packets[i]/PACKETS_AMOUNT)
-
+    loss_probability.append(result_dropped_packets[i] / NUMBER_OF_PACKETS)
 
 RO = []
 for i in range(len(aap_cases)):
-    RO.append(AST/AAP[i])
-
+    RO.append(AST / AAP[i])
 
 """plt.plot(RO, loss_probability)
 plt.title("M/M/1/1")
@@ -90,6 +95,3 @@ plt.title("M/M/1/1")
 plt.xlabel("lambda / mi")
 plt.ylabel("Worktime to uptime ratio.")
 plt.show()
-
-
-
