0c7177af78ae435a5c55fb4359d1190695608891
Piotr Sierota <P.Sierota@stud.elka.pw.edu.pl>
2019-02-01 00:32:17 +0100

    Code for server class updated with Kuba's functions.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 class Server:
-    def __init__(self):
-        self.state = "free"
+    def __init__(self, number_of_slots):
+        self.number_of_slots = number_of_slots
+        self.freeSlots = number_of_slots
         self.packets_consumed = 0
         self.packets_dropped = 0
 
@@ -11,10 +12,12 @@ class Server:
         self.packets_dropped += 1
         print("Packet dropped.")
 
-"""
     def startServingPacket(self):
-        self.state = "busy"
+        if self.freeSlots >= 1:
+            self.freeSlots -= 1
+            self.consumePacket()
+        else:
+            self.dropPacket()
 
     def stopServingPacket(self):
-        self.state = "free"
-"""
+        self.freeSlots += 1
