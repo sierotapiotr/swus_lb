9c38e38b0c6e8761ab74f2410f84b1cf3d143506
Piotr Sierota <P.Sierota@stud.elka.pw.edu.pl>
2019-02-01 02:45:38 +0100

    Changes in Server class and main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def servePacketWithDoubleThreadServer(server, pst, idle_time, t1_release_time, t2_release_time):
+def servePacketWithSingleThreadServer(arrival_time, pst, idle_time, release_time):
+    idle_time += (arrival_time - release_time)
+    release_time = arrival_time + pst
+    return [idle_time, release_time]
+
+def servePacketWithDoubleThreadServer(arrival_time, pst, idle_time, t1_release_time, t2_release_time):
     #  both threads free
     if arrival_time >= t1_release_time and arrival_time >= t2_release_time:
         if t1_release_time >= t2_release_time:
@@ -32,8 +37,7 @@ def servePacketWithDoubleThreadServer(server, pst, idle_time, t1_release_time, t
         else:
             t2_release_time = t2_release_time - (t2_release_time - t1_release_time)
     else:
-        server.dropPacket()
-
+        return False
     return [idle_time, t1_release_time, t2_release_time]
 
 
@@ -41,33 +45,44 @@ def servePacketWithDoubleThreadServer(server, pst, idle_time, t1_release_time, t
 AAP = [15, 25, 35, 45, 75, 150, 300]  # 1/LAMBDA - average arrival periods [ms]
 AST = 15  # 1/MI - average service time [ms]
 NUMBER_OF_PACKETS = 100000  # [packets]
-NUMBER_OF_SERVERS = 2
+NUMBER_OF_PACKETS_CONSUMED = 0
+NUMBER_OF_PACKETS_DROPPED = 0
+NUMBER_OF_SINGLE_THREADED_SERVERS = 2
+NUMBER_OF_DOUBLE_THREADED_SERVERS = 1
+NUMBER_OF_SERVERS = NUMBER_OF_SINGLE_THREADED_SERVERS + NUMBER_OF_DOUBLE_THREADED_SERVERS
 
-balancer = loadBalancer.LoadBalancer()
-aap_cases = []
-
-
-for i in range(len(AAP)):
-    aap_cases.append(server.Server())
+"""balancer = loadBalancer.LoadBalancer()
 
 # CREATING PACKETS
 for i in range(NUMBER_OF_PACKETS):
     packet = ipPacket.Packet(i)
-    balancer.packet_list.append(packet)
+    balancer.packet_list.append(packet)"""
 
-aap_counter = 0
 server_worktime_list = []
 server_uptime_worktime_ratio_list = []
 
-for aap_case_server in aap_cases:
+aap_counter = 0
+for aap_case in AAP:
+    # CREATE SERVERS
+    servers_single_threaded = []
+    servers_double_threaded = []
+
+    for server_machine in range(NUMBER_OF_SINGLE_THREADED_SERVERS):
+        servers_single_threaded.append(server.Server(1))
+    for server_machine in range(NUMBER_OF_DOUBLE_THREADED_SERVERS):
+        servers_double_threaded.append(server.Server(2))
+    servers = servers_single_threaded + servers_double_threaded
 
     # FOR EACH SERVER SETTING INITIAL VALUES
-    servers_release_times = []  # time of releasing the server
+    servers_release_times_thread_1 = []  # time of releasing the server
+    servers_release_times_thread_2 = []
     servers_work_times = []   # time of server being in working state
     servers_idle_times = []  # time of server being in idle state
     servers_worktime_to_uptime_ratio = []  # servers' worktime to uptime ratio
+
     for i in range(NUMBER_OF_SERVERS):
-        servers_release_times.append(0)
+        servers_release_times_thread_1.append(0)
+        servers_release_times_thread_2.append(0)
         servers_idle_times.append(0)
 
     # ARRIVALS OF PACKETS
@@ -76,22 +91,39 @@ for aap_case_server in aap_cases:
         aap_time = np.random.poisson(AAP[aap_counter])  # interval between following packets
         arrival_time += aap_time
 
-        # CHOOSING THE SERVER THAT WILL RECEIVE THE PACKET
-        if NUMBER_OF_SERVERS > 1:
-            server_number = int(round(np.random.random(NUMBER_OF_SERVERS - 1)[0]))
-        else:
-            server_number = 0
+        current_consuming_server = -1
 
-        if arrival_time >= servers_release_times[server_number]:  # dla danego serwera
-            servers_idle_times[server_number] += (arrival_time - servers_release_times[server_number])
-            aap_case_server.consumePacket()
-            pst = np.random.exponential(AST)  # packet service time
-            servers_release_times[server_number] = arrival_time + pst
-        else:
-            aap_case_server.dropPacket()
+        # CHOOSING THE SERVER THAT WILL RECEIVE THE PACKET, STARTING FROM DOUBLE-THREADED
+        for server in servers:
+            counter = 0
+            if server.isFree():
+                server.startServingPacket()
+                current_consuming_server = counter
+                NUMBER_OF_PACKETS_CONSUMED += 1
+                pst = np.random.exponential(AST)  # packet service time
+                if server.isDoubleThreaded():
+                    servePacketWithDoubleThreadServer(
+                        arrival_time,
+                        pst,
+                        servers_idle_times[current_consuming_server],
+                        servers_release_times_thread_1[current_consuming_server],
+                        servers_release_times_thread_2[current_consuming_server]
+                    )
+                else:
+                    servePacketWithSingleThreadServer(
+                    arrival_time,
+                    pst,
+                    servers_idle_times[current_consuming_server],
+                    servers_release_times_thread_1[current_consuming_server])
+                break
+            counter += 1
+        if current_consuming_server < 0:
+            NUMBER_OF_PACKETS_DROPPED += 1
+
     for server_number in range(NUMBER_OF_SERVERS):
-        servers_work_times.append(servers_release_times[server_number] - servers_idle_times[server_number])
-        servers_worktime_to_uptime_ratio.append(servers_work_times[server_number] / servers_release_times[server_number])
+        uptime = max(servers_release_times_thread_1[server_number], servers_release_times_thread_2[server_number])
+        servers_work_times.append(uptime - servers_idle_times[server_number])
+        servers_worktime_to_uptime_ratio.append(servers_work_times[server_number] / uptime)
         print("Server worktime/uptime ratio: " + str(servers_worktime_to_uptime_ratio[server_number]))
         print("Lambda/mi ratio: " + str(AST / AAP[aap_counter]))
     server_worktime_list.append(servers_work_times)
@@ -100,19 +132,19 @@ for aap_case_server in aap_cases:
 
 # RESULTS
 result_consumed_packets = []
-for i in range(len(aap_cases)):
-    result_consumed_packets.append(aap_cases[i].packets_consumed)
+for i in range(len(AAP)):
+    result_consumed_packets.append(AAP[i].packets_consumed)
 
 result_dropped_packets = []
-for i in range(len(aap_cases)):
-    result_dropped_packets.append(aap_cases[i].packets_dropped)
+for i in range(len(AAP)):
+    result_dropped_packets.append(AAP[i].packets_dropped)
 
 loss_probability = []
-for i in range(len(aap_cases)):
+for i in range(len(AAP)):
     loss_probability.append(result_dropped_packets[i] / NUMBER_OF_PACKETS)
 
 RO = []
-for i in range(len(aap_cases)):
+for i in range(len(AAP)):
     RO.append(AST / AAP[i])
 
 """plt.plot(RO, loss_probability)
